ddcd463951a342455ca54c57b77934c6905da433
norouzimohammadreza <rda0044@gmail.com>
2026-06-19 22:12:03 +0330

    create country model

---
+++ BookingHotel/Data/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingHotel.Data;
+
+
+public class Country
+{
+    public int CountryId { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+    public List<Hotel> Hotels { get; set; } = [];
+}
