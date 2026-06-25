05156758b1fb1637623323af4ad728fe38b76485
norouzimohammadreza <rda0044@gmail.com>
2026-06-25 12:35:19 +0330

    create models

---
+++ BookingHotel/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookingHotel.Models;
+
+public partial class Country
+{
+    public int CountryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string ShortName { get; set; } = null!;
+
+    public virtual ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
+}
