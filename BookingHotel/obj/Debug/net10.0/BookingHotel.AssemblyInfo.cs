242c073212068320d3c37620388ce0afcf443718
norouzimohammadreza <rda0044@gmail.com>
2026-06-25 12:35:43 +0330

    other changes

--- BookingHotel/obj/Debug/net10.0/BookingHotel.AssemblyInfo.cs
+++ BookingHotel/obj/Debug/net10.0/BookingHotel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingHotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b77e1ee27bbfe7a906d9391c16625926a85abc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingHotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingHotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
