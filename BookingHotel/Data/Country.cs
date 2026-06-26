217db4c2cc46ee82523e0acb920226b5a767fda7
norouzimohammadreza <rda0044@gmail.com>
2026-06-26 13:34:50 +0330

    validate models props

--- BookingHotel/Data/Country.cs
+++ BookingHotel/Data/Country.cs
@@ -1,10 +1,18 @@
-﻿namespace BookingHotel.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingHotel.Data;
 
 
 public class Country
 {
     public int CountryId { get; set; }
+    [Required]
+    [MaxLength(50)]
+    [MinLength(5)]
     public string Name { get; set; }
+    [Required]
+    [MaxLength(4)]
+    [MinLength(2)]
     public string ShortName { get; set; }
     public List<Hotel> Hotels { get; set; } = [];
 }
--- BookingHotel/Data/Hotel.cs
+++ BookingHotel/Data/Hotel.cs
@@ -1,10 +1,20 @@
-﻿namespace BookingHotel.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingHotel.Data;
 
 public class Hotel
 {
     public int Id { get; set; }
+    [Required]
+    [MaxLength(60)]
+    [MinLength(4)]
     public string Name { get; set; }
+    [Required]
+    [MaxLength(120)]
+    [MinLength(5)]
     public string Address { get; set; }
+    [Required]
+    [Range(0,5)]
     public double Rating { get; set; }
     
     public int CountryId { get; set; }
