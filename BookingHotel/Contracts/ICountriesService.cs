01b953d1baed583004465f28868c578cf75e605a
norouzimohammadreza <rda0044@gmail.com>
2026-06-28 00:44:40 +0330

    create new interface for countries

---
+++ BookingHotel/Contracts/ICountriesService.cs
@@ -0,0 +1,12 @@
+﻿using BookingHotel.DTOs.Country;
+
+namespace BookingHotel.Contracts;
+
+public interface ICountriesService
+{
+    Task<IEnumerable<GetCountriesDto>> GetCountries(); 
+    Task<GetCountryDto?> GetCountry(int countryId);
+    Task<GetCountryDto> UpdateCountryDto(int countryId, UpdateCountryDto countryDto);
+    Task<GetCountryDto> CreateCountryDto(CreateCountryDto countryDto);
+    Task DeleteCountryDto(int countryId);
+} 
