a1197e04431b3f9516791ce98cbae2dcdd04d9d1
norouzimohammadreza <rda0044@gmail.com>
2026-06-30 00:25:46 +0330

    create interface for hotels service

---
+++ BookingHotel/Contracts/IHotelsService.cs
@@ -0,0 +1,12 @@
+﻿using BookingHotel.DTOs.Hotel;
+
+namespace BookingHotel.Contracts;
+
+public interface IHotelsService
+{
+    Task<IEnumerable<GetHotelsDto>> GetHotels();
+    Task<GetHotelDto?> GetHotel(int id);
+    Task<GetHotelDto> CreateHotel(CreateHotelDto hotelDto);
+    Task UpdateHotel(int id, UpdateHotelDto hotelDto);
+    Task DeleteHotel(int id);
+}
