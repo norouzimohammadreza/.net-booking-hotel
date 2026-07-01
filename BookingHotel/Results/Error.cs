1c1e86d9549c2cd16ff19bb56da222abd9593959
norouzimohammadreza <rda0044@gmail.com>
2026-07-02 02:06:22 +0330

    create error record struct for result pattern

--- BookingHotel/Results/Error.cs
+++ BookingHotel/Results/Error.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookingHotel.Results;
 
-public readonly record struct Error(string Code, string Message)
+public readonly record struct Error(string Code, string Description)
 {
     public static readonly Error None = new ("","");
     public bool IsNone => string.IsNullOrWhiteSpace(Code);
