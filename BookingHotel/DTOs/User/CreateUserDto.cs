455f549528272738f96c739d708cf0385f0badcd
norouzimohammadreza <rda0044@gmail.com>
2026-07-04 01:18:14 +0330

    create a new dto for creating user

---
+++ BookingHotel/DTOs/User/CreateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingHotel.DTOs.User;
+
+public class CreateUserDto
+{
+    [Required]
+    [EmailAddress]
+    [MaxLength(80)]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(8)]
+    [MaxLength(20)]
+    public string Password { get; set; }
+}
