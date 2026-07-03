1a9eb38aaa4c8232d8c7bb2b225017fb2081054d
norouzimohammadreza <rda0044@gmail.com>
2026-07-04 01:18:48 +0330

    create user service for auth

---
+++ BookingHotel/Contracts/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿using BookingHotel.DTOs.User;
+using BookingHotel.Results;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookingHotel.Contracts;
+
+public interface IUsersService
+{
+     Task<Result<GetUserDto>> Register(CreateUserDto createUserDto);
+     Task<Result<string>> Login(LoginUserDto loginUserDto);
+     Task<string> GenerateToken(IdentityUser user);
+}
---
+++ BookingHotel/Services/UsersService.cs
@@ -0,0 +1,82 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BookingHotel.Contracts;
+using BookingHotel.DTOs.User;
+using BookingHotel.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace BookingHotel.Services;
+
+public class UsersService(UserManager<IdentityUser> userManager, IConfiguration configuration) : IUsersService
+{
+    public async Task<Result<GetUserDto>> Register(CreateUserDto createUserDto)
+    {
+        var userModel = new IdentityUser
+        {
+            Email = createUserDto.Email,
+            UserName = createUserDto.Email,
+        };
+        var result = await userManager.CreateAsync(userModel,createUserDto.Password);
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(error => new Error("BadRequest", error.Description)).ToArray();
+            return Result<GetUserDto>.Failure(errors);
+        }
+
+        var registeredUserDto = new GetUserDto
+        {
+            Id = userModel.Id,
+            Email = userModel.Email
+        };
+        return Result<GetUserDto>.Success(registeredUserDto);
+    }
+
+    public async Task<Result<string>> Login(LoginUserDto loginUserDto)
+    {
+        var user = await userManager.FindByEmailAsync(loginUserDto.Email);
+
+        if (user == null)
+        {
+            return Result<string>.Failure(new Error("BadRequest","User not found"));
+        }
+
+        var valid = await userManager.CheckPasswordAsync(user, loginUserDto.Password);
+        if (valid == false)
+        {
+            return Result<string>.Failure(new Error("BadRequest", "Invalid login"));
+        }
+        var token = await GenerateToken(user);
+        return Result<string>.Success(token);
+    }
+
+    public async Task<string> GenerateToken(IdentityUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id), 
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!), 
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.PreferredUsername , user.Email!)
+        };
+        var roles = await userManager.GetRolesAsync(user);
+        var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+        claims = claims.Union(roleClaims).ToList();
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JwtSettings:Issuer"],
+            audience: configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(configuration["JwtSettings:DurationInMinutes"])),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
