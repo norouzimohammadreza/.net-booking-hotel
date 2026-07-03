8e3f9278f0e90943deab14c094d0661634eb9004
norouzimohammadreza <rda0044@gmail.com>
2026-07-04 01:19:23 +0330

    create controller for auth

---
+++ BookingHotel/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using BookingHotel.Contracts;
+using BookingHotel.DTOs.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotel.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[AllowAnonymous]
+public class AuthController(IUsersService usersService) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<GetUserDto>> Register(CreateUserDto createUserDto)
+    {
+       var result = await usersService.Register(createUserDto);
+       if (!result.IsSuccess)
+       {
+           return MapErrorsToResponse(result.Errors);
+       }
+       return ToActionResult(result);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(LoginUserDto loginUserDto)
+    {
+        var result = await usersService.Login(loginUserDto);
+        if (!result.IsSuccess)
+        {
+           return MapErrorsToResponse(result.Errors);
+        }
+        return ToActionResult(result);
+    }
+}
