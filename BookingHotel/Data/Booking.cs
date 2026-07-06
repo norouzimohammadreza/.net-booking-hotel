99f6a10b5e0720c24f452c770343236e03575f1d
norouzimohammadreza <rda0044@gmail.com>
2026-07-06 22:16:10 +0330

    create hotel admin model

---
+++ BookingHotel/Data/Booking.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingHotel.Data;
+
+public class Booking
+{
+    public int Id { get; set; }
+    
+    public Hotel? Hotel { get; set; }
+    public int HotelId { get; set; }
+    
+    public IdentityUser? User { get; set; }
+    public required string UserId { get; set; }
+    
+    
+}  
---
+++ BookingHotel/Data/HotelAdmin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingHotel.Data;
+
+public class HotelAdmin
+{
+    public int Id{ get; set; }
+    
+    public IdentityUser? User { get; set; }
+    public required string UserId { get; set; } 
+
+    public Hotel? Hotel { get; set; }
+    public int HotelId { get; set; } 
+    public DateOnly CheckIn { get; set; }
+    public DateOnly CheckOut { get; set; }
+    public int Guests { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }     
+    public string Status = "Pending, Confirmed, Cancelled";
+}
