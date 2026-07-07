a98e28010fcb3547bdc71fc47978c971ae0f847e
norouzimohammadreza <rda0044@gmail.com>
2026-07-07 22:57:10 +0330

    create empty controller for handle booking

---
+++ BookingHotel/Controllers/HotelBookingsController.cs
@@ -0,0 +1,20 @@
+﻿using BookingHotel.DTOs.Booking;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotel.Controllers;
+
+[ApiController]
+[Route("api/hotels/{hotelId}/bookings")]
+public class HotelBookingsController : Controller
+{
+    [HttpGet]
+    public async Task<ActionResult<GetBookingDto>> GetBookings([FromRoute] int hotelId)
+    {
+        return Ok();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<GetBookingDto>> Create([FromRoute] int hotelId,[FromBody] CreateBookingDto createBookingDto){
+        return Ok();
+    }
+} 
