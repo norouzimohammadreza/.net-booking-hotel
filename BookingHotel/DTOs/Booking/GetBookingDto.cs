4157b649936605ab71ac9a2287e15598f962a36c
norouzimohammadreza <rda0044@gmail.com>
2026-07-07 22:56:32 +0330

    create get booking dto

---
+++ BookingHotel/DTOs/Booking/GetBookingDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BookingHotel.DTOs.Booking;
+
+public record GetBookingDto
+(
+    int Id,
+    int HotelId,
+    string HotelName,
+    DateOnly CheckIn,
+    DateOnly CheckOut,
+    int Guest,
+    decimal TotalPrice,
+    string Status,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt
+);
