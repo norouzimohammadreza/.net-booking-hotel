89edc44442e82f41fe061f77292bce923d78cb14
norouzimohammadreza <rda0044@gmail.com>
2026-07-07 22:42:13 +0330

    booking configuration

--- BookingHotel/Data/BookingHotelDbContext.cs
+++ BookingHotel/Data/BookingHotelDbContext.cs
@@ -12,6 +12,7 @@ public partial class BookingHotelDbContext(DbContextOptions<BookingHotelDbContex
 
     public virtual DbSet<Hotel> Hotels { get; set; }
     public virtual DbSet<HotelAdmin> HotelAdmins { get; set; }
+    public virtual DbSet<Booking> Bookings { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
---
+++ BookingHotel/Data/Configurations/BookingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingHotel.Data.Configurations;
+
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.Property(x => x.Status)
+            .HasConversion<string>()
+            .HasMaxLength(20);
+        builder.HasIndex(x => x.UserId);
+        builder.HasIndex(x => x.HotelId);
+        builder.HasIndex(x => new { x.CheckIn, x.CheckOut });
+    }
+}
