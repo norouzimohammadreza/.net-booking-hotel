b7490c9277de334768d5998dabc192860de85535
norouzimohammadreza <rda0044@gmail.com>
2026-07-15 18:14:39 +0330

    create filter for dectecting hotel admin and system admin

---
+++ BookingHotel/AuthorizationFilters/HotelOrSystemAdminAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotel.AuthorizationFilters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method , AllowMultiple = false)]
+public class HotelOrSystemAdminAttribute : TypeFilterAttribute
+{
+    public HotelOrSystemAdminAttribute() : base(typeof(HotelOrSystemAdminFilter))
+    {
+    }
+} 
---
+++ BookingHotel/AuthorizationFilters/HotelOrSystemAdminFilter.cs
@@ -0,0 +1,47 @@
+﻿using BookingHotel.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace BookingHotel.AuthorizationFilters;
+
+public class HotelOrSystemAdminFilter(BookingHotelDbContext dbContext):IAsyncAuthorizationFilter
+{
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var httpUser = context.HttpContext.User;
+        if (httpUser?.Identity?.IsAuthenticated == false)
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+        if (httpUser!.IsInRole("Admin"))
+        {
+            return;
+        }
+
+        var userId = httpUser.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+        
+        context.RouteData.Values.TryGetValue("hotelId", out var hotelIdObj);
+        int.TryParse(hotelIdObj?.ToString(),out int hotelId );
+        if (hotelId == 0)
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+        
+        var isHotelAdmin = await dbContext.HotelAdmins.AnyAsync(q=> q.HotelId == hotelId && q.UserId == userId);
+        if (!isHotelAdmin)
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+        
+    }
+}
