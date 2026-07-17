6ae451def3f36b91a6d2f6ddd0545bf99c17d074
norouzimohammadreza <rda0044@gmail.com>
2026-07-18 00:06:40 +0330

    remove attributes validation for hotel dtos

--- BookingHotel/DTOs/Hotel/UpdateHotelDto.cs
+++ BookingHotel/DTOs/Hotel/UpdateHotelDto.cs
@@ -4,20 +4,12 @@ namespace BookingHotel.DTOs.Hotel;
 
 public class UpdateHotelDto
 {
-    [Required]
     public int Id { get; set; }
-    [Required]
-    [MaxLength(60)]
-    [MinLength(4)]
+
     public string Name { get; set; }
 
-    [Required]
-    [MaxLength(120)]
-    [MinLength(5)]
     public string Address { get; set; }
 
-    [Required] 
-    [Range(0, 5)] 
     public double Rating { get; set; }
 
     public int CountryId { get; set; }
