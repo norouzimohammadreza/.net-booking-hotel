0997f8e12936f95ca88085dbfd087b4fb0b61735
norouzimohammadreza <rda0044@gmail.com>
2026-07-17 23:19:33 +0330

    create validator for countries post and put requests

---
+++ BookingHotel/Validators/Country/CreateCountryDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using BookingHotel.DTOs.Country;
+using FluentValidation;
+
+namespace BookingHotel.Validators.Country;
+
+public class CreateCountryDtoValidator:AbstractValidator<CreateCountryDto>
+{
+    public CreateCountryDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(5, 50);
+
+        RuleFor(x => x.ShortName)
+            .NotEmpty()
+            .Length(2, 4);
+    }
+}
---
+++ BookingHotel/Validators/Country/UpdateCountryDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using BookingHotel.DTOs.Country;
+using FluentValidation;
+
+namespace BookingHotel.Validators.Country;
+
+public class UpdateCountryDtoValidator :AbstractValidator<UpdateCountryDto>
+{
+    public UpdateCountryDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(5, 50);
+
+        RuleFor(x => x.ShortName)
+            .NotEmpty()
+            .Length(2, 4);
+    }
+}
