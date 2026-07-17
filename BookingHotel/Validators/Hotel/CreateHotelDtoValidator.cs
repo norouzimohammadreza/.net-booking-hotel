72d7ca69ae5b31f70c2ab0cc26989b6d10d5c66e
norouzimohammadreza <rda0044@gmail.com>
2026-07-18 00:05:15 +0330

    create validators for hotels post and put requests

---
+++ BookingHotel/Validators/Hotel/CreateHotelDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using BookingHotel.DTOs.Hotel;
+using FluentValidation;
+
+namespace BookingHotel.Validators.Hotel;
+
+public class CreateHotelDtoValidator: AbstractValidator<CreateHotelDto>
+{
+    public CreateHotelDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(4, 60);
+
+        RuleFor(x => x.Address)
+            .NotEmpty()
+            .Length(5, 120);
+
+        RuleFor(x => x.Rating)
+            .GreaterThanOrEqualTo(0)
+            .LessThanOrEqualTo(5);
+
+        RuleFor(x => x.CountryId)
+            .GreaterThan(0);
+    }
+}
---
+++ BookingHotel/Validators/Hotel/UpdateHotelDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using BookingHotel.DTOs.Hotel;
+using FluentValidation;
+
+namespace BookingHotel.Validators.Hotel;
+
+public class UpdateHotelDtoValidator : AbstractValidator<UpdateHotelDto>
+{
+    public UpdateHotelDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(4, 60);
+
+        RuleFor(x => x.Address)
+            .NotEmpty()
+            .Length(5, 120);
+
+        RuleFor(x => x.Rating)
+            .GreaterThanOrEqualTo(0)
+            .LessThanOrEqualTo(5);
+
+        RuleFor(x => x.CountryId)
+            .GreaterThan(0);
+    }
+}
