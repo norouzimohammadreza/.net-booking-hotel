cc9637f9388b8c8e2073b25fc3fe0f96fc90e4ef
norouzimohammadreza <rda0044@gmail.com>
2026-07-18 18:27:42 +0330

    set private for props set and create constructor and create update method

--- BookingHotel/Entities/Hotel.cs
+++ BookingHotel/Entities/Hotel.cs
@@ -2,20 +2,49 @@
 
 public class Hotel
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public string Name { get; set; }
+    public string Name { get; private set; }
 
-    public string Address { get; set; }
+    public string Address { get; private set; }
 
-    public double Rating { get; set; }
+    public double Rating { get; private set; }
+
+    public decimal PerNightRating { get; private set; }
+
+    public int CountryId { get; private set; }
+
+    public Country? Country { get; private set; }
+
+    public ICollection<HotelAdmin> Admins { get; private set; } = [];
+
+    public ICollection<Booking> Bookings { get; private set; } = [];
     
-    public decimal PerNightRating { get; set; }
+    public Hotel(
+        string name,
+        string address,
+        double rating,
+        decimal perNightRating,
+        int countryId)
+    {
+        Name = name;
+        Address = address;
+        Rating = rating;
+        PerNightRating = perNightRating;
+        CountryId = countryId;
+    }
     
-    public int CountryId { get; set; }
-    
-    public Country? Country { get; set; }
-    
-    public ICollection<HotelAdmin> Admins { get; set; } = [];
-    public ICollection<Booking> Bookings { get; set; } = [];
+    public void Update(
+        string name,
+        string address,
+        double rating,
+        decimal perNightRating,
+        int countryId)
+    {
+        Name = name;
+        Address = address;
+        Rating = rating;
+        PerNightRating = perNightRating;
+        CountryId = countryId;
+    }
 }
