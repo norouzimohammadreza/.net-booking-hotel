9c283bf609ee58ed6ba468d84105f5a50946a909
norouzimohammadreza <rda0044@gmail.com>
2026-07-19 10:54:29 +0330

    private set props in hotel admin

--- BookingHotel/Entities/HotelAdmin.cs
+++ BookingHotel/Entities/HotelAdmin.cs
@@ -5,11 +5,19 @@ namespace BookingHotel.Entities;
 
 public class HotelAdmin
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public IdentityUser? User { get; set; }
-    public required string UserId { get; set; }
+    public IdentityUser? User { get; private set; }
+    public string UserId { get; private set; }
 
-    public Hotel? Hotel { get; set; }
-    public int HotelId { get; set; }
+    public Hotel? Hotel { get; private set; }
+    public int HotelId { get; private set; }
+    
+    private HotelAdmin(){}
+
+    public HotelAdmin(int hotelId, string userId)
+    {
+        HotelId = hotelId;
+        UserId = userId;
+    }
 }
